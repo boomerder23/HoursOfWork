ade9e3abc800c52bc8d9239b234695a1db854096
ArneK <boomerder23@gmail.com>
2017-10-24 18:13:25 +0200

    neue Klassen geschrieben und testDatei hinzugefügt

---
+++ HoursOfWorkLib/Import.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoursOfWorkLib
+{
+    public class Import
+    {
+        #region Constructors
+
+        public Import()
+        {
+            
+        }
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Methods
+
+        public List<TimeDataset> ImportCSVFile(String path)
+        {
+            List<TimeDataset> data = new List<TimeDataset>();
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+
+                // Read and display lines from the file until the end of 
+                // the file is reached.
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    String line = sr.ReadLine();
+                    String[] values = line.Split(',');
+
+                    TimeDataset tempDataset = new TimeDataset();
+                    tempDataset.Date = Convert.ToDateTime(values[0]);
+
+                    data.Add(tempDataset);
+                }
+            }
+
+            return data;
+        }
+
+        #endregion
+    }
+}
---
+++ HoursOfWorkLib/TimeDataset.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoursOfWorkLib
+{
+    public class TimeDataset
+    {
+        #region Constructors
+
+        public TimeDataset()
+        {
+
+        }
+
+        #endregion
+
+        #region Properties
+
+        private Int32 id;
+
+        public Int32 Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        private Int32 calenderWeek;
+
+        public Int32 CalenderWeek
+        {
+            get { return calenderWeek; }
+            set { calenderWeek = value; }
+        }
+
+        private DateTime date;
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+
+        private DateTime begin;
+
+        public DateTime Begin
+        {
+            get { return begin; }
+            set { begin = value; }
+        }
+
+        private DateTime end;
+
+        public DateTime End
+        {
+            get { return end; }
+            set { end = value; }
+        }
+
+        private TimeSpan totalTime;
+
+        public TimeSpan TotalTime
+        {
+            get { return totalTime; }
+            set { totalTime = value; }
+        }
+
+        private String comment;
+
+        public String Comment
+        {
+            get { return comment; }
+            set { comment = value; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+    }
+}
